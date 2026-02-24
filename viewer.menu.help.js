e2d388b9b966d9ebb772407f7a298e516deb9b08
andric31 <andric31@gmail.com>
2026-02-24 23:57:02 +0100

    Add files via upload

--- viewer.menu.help.js
+++ viewer.menu.help.js
@@ -15,7 +15,7 @@
 
       
       .mh-subpanel{
-        margin:4px 0 16px 42px;
+        margin:2px 0 8px 42px;
         padding:10px 14px;
         border-radius:10px;
         background:rgba(255,255,255,0.04);

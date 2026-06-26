ea3eb9fb29a2b94f55906e0271bc5ba2b84cbdf2
andric31 <andric31@gmail.com>
2026-06-26 16:51:24 +0200

    Add files via upload

--- auth/auth.client.js
+++ auth/auth.client.js
@@ -167,7 +167,7 @@
           window.ViewerMenu.addItem('🎮 Mes jeux', () => { window.open('/compte/mes-jeux.html', '_blank', 'noopener'); });
           const itemsMesJeux = window.ViewerMenu.__getItems?.();
           if (itemsMesJeux?.[itemsMesJeux.length - 1]) itemsMesJeux[itemsMesJeux.length - 1].__authManaged = true;
-          window.ViewerMenu.addItem('⭐ Game+', () => { window.open('/game+/', '_blank', 'noopener'); });
+          window.ViewerMenu.addItem('✦✦✦ Game+', () => { window.open('/game+/', '_blank', 'noopener'); });
           const itemsGamePlus = window.ViewerMenu.__getItems?.();
           if (itemsGamePlus?.[itemsGamePlus.length - 1]) itemsGamePlus[itemsGamePlus.length - 1].__authManaged = true;
           window.ViewerMenu.addItem(logoutLabel, async () => {
